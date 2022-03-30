84e3d06a83c5009cf5deab1bb6fe0b595aafdd61
sjumaa93 <sjumaa93@gmail.com>
2022-03-30 19:07:42 +0100

    xpaths added

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
-import selenium
+from typing import final
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 from selenium.webdriver import Chrome
 driver = Chrome('./chromedriver')
 
-url = 'https://www.amazon.co.uk/s?k=mobile+phones'
+url = 'https://www.amazon.co.uk/'
 driver.get(url)
 
-buttons = driver.find_element_by_xpath('//a[@type="button"]')
+driver.implicitly_wait(10)
+driver.find_element(By.XPATH,"//span[contains(@class,'on-primary')]").click()
+driver.find_element(By.XPATH,"//input[contains(@id, 'search')]").send_keys("Mobile Phones")
+driver.find_element(By.XPATH,"//input[@value='Go']").click()
 
-for button in buttons:
-    if button.text == "Accept Cookies":
-        relevant_button = button
+phone_name = driver.find_elements(By.XPATH,"//span[contains(@class, 'a-size-medium a-color-base a-text-normal')]")
+phone_price = driver.find_elements(By.XPATH,"//span[contains(@class, 'a-price-whole')]")
 
-    relevant_button.click()
+myphone=[]
+myprice=[]
+
+for phone in phone_name:
+    print(phone.text)
+    myphone.append(phone.text)
+
+print("*"*50)
+
+for price in phone_price:
+    print(price.text)
+    myprice.append(price.text)
+
+final_list = zip(myphone,myprice)
+
+for data in list(final_list):
+    print(data)
