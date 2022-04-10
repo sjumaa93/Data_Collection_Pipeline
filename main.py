fb121b814fe079ec80580e47290da98a6a511438
sjumaa93 <sjumaa93@gmail.com>
2022-04-10 02:02:47 +0100

    updated dictionaries with data

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 
-if __name__ == '__main__':
+def main():
     import scraper
     bot = scraper.Scraper('https://www.amazon.co.uk')
     bot.accept_cookies("//span[contains(@class,'on-primary')]")
     bot.search_site("//input[contains(@id, 'search')]",'Mobile Phones')
-    bot.go_button("//input[@value='Go']")
-    bot.findproducts("//span[contains(@class, 'a-size-medium a-color-base a-text-normal')]")
+    bot.click_go_button("//input[@value='Go']")
+    bot.find_links(
+        "//h2[@class='a-size-mini a-spacing-none a-color-base s-line-clamp-2']", 
+        "//span[contains(@class, 'a-price-whole')]",
+        "//span[contains(@class, 'a-size-large product-title-word-break')]"
+        )
+    bot.close_driver
+    
 
+if __name__ == '__main__':
+    main()
--- scraper.py
+++ scraper.py
@@ -25,17 +25,49 @@ class Scraper:
         searchbox.send_keys(query)
         searchbox.click()
 
-    def go_button(self,xpath):
+    def click_go_button(self,xpath):
         time.sleep(1)
         self.driver.find_element(By.XPATH, xpath).click()
 
-    def findproducts(self,xpath):
-        time.sleep(5)
-        myphone=[]
-        product_name = self.driver.find_elements(By.XPATH, xpath)
-        for title in product_name:
-            myphone.append(title.text)
-        print(myphone)
+    def find_links(self, xp_html, xp_price, xp_title):
+        time.sleep(2)
+        link_list = []
+        container = self.driver.find_elements(By.XPATH, xp_html)
+        for link in container:
+            link_list.append(link.find_element(By.TAG_NAME, 'a').get_attribute('href'))
+        
+        print(link_list)
+        
+        dict_list = {
+            'Link':[],
+            'Name':[],
+            'Price':[]
+            }
+        for link in link_list[0:4]:
+            self.driver.get(link)
+            time.sleep(2)
+            dict_list['Link'].append(link)
+            price = self.driver.find_element(By.XPATH, xp_price)
+            dict_list['Price'].append(price)
+            name = self.driver.find_element(By.XPATH, xp_title)
+            dict_list['Name'].append(name)
+        
+        print(dict_list)
+
+    def close_driver(self):
+        self.driver.quit()
 
 
+    #def find_product_title(self,xpath):
+    #    time.sleep(5)
+    #    link_list=[]
+    #    product_name = self.driver.find_element(By.XPATH, xpath).__getattribute__('href')
+    #    for title in product_name:
+    #        link_list.append(title.text)
+    #    print(link_list)
 
+    #def find_product_title(self, xpath):
+    #    self.container = self.driver.find_element(By.XPATH, xpath)
+
+
+    # div container - div class = "a-section a-spacing-none s-padding-right-small s-title-instructions-style"
