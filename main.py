21e56038a21fb0d78953a5a90c9054fa38ba0166
sjumaa93 <sjumaa93@gmail.com>
2022-05-10 19:59:08 +0100

    connect to database and upload to s3 bucket

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 
 def main():
     import scraper
-    bot = scraper.Web_Scraper('https://www.amazon.co.uk')
-    bot.accept_cookies("//span[contains(@class,'on-primary')]")
-    bot.search_site("//input[contains(@id, 'search')]",'Mobile Phones')
-    bot.click_go_button("//input[@value='Go']")
-    bot.find_data(
-        "//h2[@class='a-size-mini a-spacing-none a-color-base s-line-clamp-2']",
-        "//span[contains(@class, 'a-size-large product-title-word-break')]",
-        "//span[contains(@class, 'a-price-whole')]",
-        #"//span[contains(@class, 'imgTagWrapper')]",
-        )
-
-    bot.create_json
-    bot.close_driver()
+    scraper = scraper.Scraper()
+    scraper.accept_cookies()
+    scraper.search_site()
+    scraper.click_go_button()
+    scraper.find_links()
+    scraper.create_dict()
+    scraper.create_json()
+    scraper.close_driver()
 
 if __name__ == '__main__':
     main()
--- scraper.py
+++ scraper.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
-#from webdriver_manager.chrome import ChromeDriverManager
+
 
 import time
 import pandas as pd
@@ -16,7 +16,37 @@ import uuid
 import json
 import urllib.request
 
+import sqlalchemy
+from sqlalchemy import create_engine
+import boto3
+import tempfile
+
 class Scraper:
+    def __init__(self, url:str ='https://www.amazon.co.uk'):
+        self.url = url
+        #self.driver = Chrome(ChromeDriveManager().install())
+        self.driver = Chrome('./chromedriver')
+        self.driver.get(url)
+        self.link_list = []
+        self.product_data = {
+            'Link':[],
+            'Name':[],
+            'Price':[],
+            'UUID':[],
+            }
+
+
+        DATABASE_TYPE = 'postgresql'
+        DBAPI = 'psycopg2'
+        HOST = 'database.cqn1abbztdx3.us-east-1.rds.amazonaws.com'
+        USER = 'postgres'
+        PASSWORD = 'Blackberry12'
+        DATABASE = 'postgres'
+        PORT = 5432
+
+        self.engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+        self.client = boto3.client('s3')
+
     '''
     This class is a scraper that can be used to retrieve data from different websites
 
@@ -28,13 +58,16 @@ class Scraper:
     driver:
         The webdriver object
     '''
-    def __init__(self, url:str ='https://www.amazon.co.uk'):
-        self.url = url
-        #self.driver = Chrome(ChromeDriveManager().install())
-        self.driver = Chrome('./chromedriver')
-        self.driver.get(url)
+
 
     def accept_cookies(self, xpath: str = "//span[contains(@class,'on-primary')]"):
+        try:
+            time.sleep(1)
+            self.driver.find_element(By.XPATH, xpath).click()
+            print('Cookies Accepted ✅')
+        except:
+            print('No cookies found')
+
         '''
         This method looks for and clicks the accept cookies button
 
@@ -42,13 +75,12 @@ class Scraper:
         xpath: str
             The xpass of the cookies button
         '''
-        try:
-            time.sleep(1)
-            self.driver.find_element(By.XPATH, xpath).click()
-        except:
-            print('No cookies found')
 
     def search_site(self, xpath: str = "//input[contains(@id, 'search')]", query: str = 'Mobile Phones'):
+        time.sleep(1)
+        searchbox = self.driver.find_element(By.XPATH, xpath)
+        searchbox.send_keys(query)
+        
         '''
         This method finds the search bar and enters a search query
 
@@ -58,11 +90,12 @@ class Scraper:
         query: str
             This is the query we want to search for
         '''
-        time.sleep(1)
-        searchbox = self.driver.find_element(By.XPATH, xpath)
-        searchbox.send_keys(query)
 
     def click_go_button(self,xpath: str= "//input[@value='Go']"):
+        time.sleep(1)
+        self.driver.find_element(By.XPATH, xpath).click()
+        print('Searching site ⌛')
+
         '''
         This method searches for the 'go' button on the site
 
@@ -70,33 +103,44 @@ class Scraper:
         xpath: str
             This is the xpath of the 'go' button
         '''
-        time.sleep(1)
-        self.driver.find_element(By.XPATH, xpath).click()
 
     def find_links(self, xp_html: str= "//h2[@class='a-size-mini a-spacing-none a-color-base s-line-clamp-2']"):
+        time.sleep(2)
+        container = self.driver.find_elements(By.XPATH, xp_html)
+        
+        #json_file = open('raw_data.json','r')
+        #json_data = json_file.read()
+
+        for link in container:
+            self.link_list.append(link.find_element(By.TAG_NAME, 'a').get_attribute('href'))
+
         '''
         This method finds all the links on the page and stores them in a list
 
         Parameters:
         xpath: str
         This is the xpath of the links on the page
-
-        Returns:
-        link_list: list
-        This is a list of the urls on the page
         '''
-        time.sleep(2)
-        link_list = []
-        container = self.driver.find_elements(By.XPATH, xp_html)
-        for link in container:
-            link_list.append(link.find_element(By.TAG_NAME, 'a').get_attribute('href'))
-        return link_list
-
     
     def create_dict(self, 
                     xp_title: str="//span[contains(@class, 'a-size-large product-title-word-break')]", 
                     xp_price: str="//span[contains(@class, 'a-price-whole')]",
                     xp_image: str= "//*[@id='landingImage']"):
+        print('Collecting Product Data ⌛')
+        for link in self.link_list[0:10]:
+            self.driver.get(link)
+            time.sleep(2)
+            self.product_data['Link'].append(link)
+            name = self.driver.find_element(By.XPATH, xp_title).text
+            self.product_data['Name'].append(name)
+            price = self.driver.find_element(By.XPATH, xp_price).text
+            self.product_data['Price'].append(price)
+            id = str(uuid.uuid4())
+            self.product_data['UUID'].append(id)
+            image_url = self.driver.find_element(By.XPATH, xp_image).get_attribute('src')
+            urllib.request.urlretrieve(image_url, f"images/{id}.jpg")
+            print('Product Scraped Successfully ✅')
+
         '''
         This creates a dictionary, and adds the name, price and unique identifier to each entry
 
@@ -106,40 +150,14 @@ class Scraper:
 
         xp_price: str
         This is the xpath of the product price on the page
-
-        Returns:
-        dict_list: dictionary
-        This is a dictionary containing url, title, price and unique identifier of each page
         '''
-        link_list = self.find_links()
-        dict_list = {
-            'Link':[],
-            'Name':[],
-            'Price':[],
-            'UUID':[],
-            'image_url':[]
-            }
-
-        for link in link_list[0:4]:
-            self.driver.get(link)
-            time.sleep(2)
-            dict_list['Link'].append(link)
-            name = self.driver.find_element(By.XPATH, xp_title).text
-            dict_list['Name'].append(name)
-            price = self.driver.find_element(By.XPATH, xp_price).text
-            dict_list['Price'].append(price)
-            id = uuid.uuid4()
-            dict_list['UUID'].append(id)
-            image_url = self.driver.find_element(By.XPATH, xp_image).get_attribute('src')
-            urllib.request.urlretrieve(image_url, f"{id}.jpg")
-
-        
-        print(pd.DataFrame(dict_list))
-        print(dict_list)
-
-        return dict_list
+            
     
     def create_json(self):
+        with open(f"raw_data.json", "w") as f:
+                    json.dump(self.product_data, f)
+        print('Saved as raw_data.json 💾')
+
         '''
         This method takes the dictionary and posts it as a json file
 
@@ -147,11 +165,17 @@ class Scraper:
         json.dumps:
             The object converts a python object into an equivalent JSON object
         '''
-        dictionary = self.create_dict()
-        file = json.dumps(dictionary)
-        with open('file.json', 'w') as f:
-            f.write(file)
-            f.close()
+    
+    def upload_images(self, src):
+        pass
+
+    def data_to_sql(self):
+        with open('./raw_data.json', 'r') as filename:
+            data = json.load(filename)
+        df = pd.DataFrame(data)
+        df.columns = df.columns.str.lower()
+        self.engine.connect()
+        df.to_sql('raw_data', con=self.engine, if_exists='replace')
 
     def close_driver(self):
         self.driver.quit()
