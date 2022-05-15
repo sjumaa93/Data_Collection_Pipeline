65596d917634f1360ed5ad730a9f963d935dd6aa
sjumaa93 <sjumaa93@gmail.com>
2022-05-15 20:08:44 +0100

    refactoring

--- test_scraper.py
+++ test_scraper.py
@@ -2,13 +2,14 @@ import unittest
 import scraper
 from selenium.webdriver.common.by import By
 
+
 class TestScraper(unittest.TestCase):
     def setUp(self):
         self.bot = scraper.Scraper()
 
     def test_accept_cookies(self):
         self.bot.accept_cookies()
-    
+
     def test_search_site(self):
         self.bot.search_site()
 
