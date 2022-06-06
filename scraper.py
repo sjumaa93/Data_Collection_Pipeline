7e5568f694af1c63e125f0e01985eff982579348
sjumaa93 <sjumaa93@gmail.com>
2022-06-06 21:21:37 +0100

    docker

--- scraper.py
+++ scraper.py
@@ -32,7 +32,13 @@ class Scraper:
         '''
         self.url = url
         options = Options()
-        # options.add_argument('--headless')
+        options.add_argument("--disable-gpu")
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument("--start-maximized")
+        options.add_argument('--remote-debugging-port=9222')
+        options.add_argument("user-agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'")
 
         self.driver = Chrome(ChromeDriverManager().install(), options=options)
         # self.driver = Chrome('./chromedriver')
--- test_scraper.py
+++ test_scraper.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
 
-
 class TestScraper(unittest.TestCase):
     def setUp(self):
         self.bot = scraper.Scraper()
@@ -14,12 +13,13 @@ class TestScraper(unittest.TestCase):
         with self.assertRaises(NoSuchElementException):
             self.bot.driver.find_element_by_xpath(
                 '//*[@id="sp-cc-accept"]')
-        # self.bot.driver.find_element(By.XPATH, '//*[@id="nav-logo-sprites"]')
+        # checks if cookie button is still there
 
     def test_search_site(self):
         self.bot.accept_cookies()
         self.bot.search_site()
-        self.bot.driver.find_element(By.XPATH,"//span[contains(@class,'a-size-medium')]")
+        self.bot.driver.find_element(By.XPATH, "//span[contains(@class,'a-size-medium')]")
+        # checks we are on the search result page
 
     def tearDown(self):
         pass
